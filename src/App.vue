ef2cd32f2fbfbc0c5937a24896e4f56086acfbdd
Tygo <tygoee@outlook.com>
2026-02-15 15:24:28 +0100

    Fixed wrong default page

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { pages } from './components/pages';
 
 import ResourceSidebar from './components/ResourceSidebar.vue';
 
-const resources = ref<Resource[]>([{ id: 0, type: 'dummy-page', name: '' }]);
+const resources = ref<Resource[]>([{ id: 0, type: 'default-page', name: 'No resource selected' }]);
 const selectedPage = ref<number>(0);
 </script>
 
@@ -24,11 +24,41 @@ const selectedPage = ref<number>(0);
 
 <style>
 #app {
+  display: grid;
+  grid-template-columns: 300px 1fr;
+
   font-size: 15px;
   font-family: Arial, Helvetica, sans-serif;
   color: var(--text-color);
   background-color: var(--body-color);
 
   height: 100vh;
+}
+
+h1,
+h2 {
+  font-size: 15px;
+}
+
+a {
+  color: inherit;
+  text-decoration: inherit;
+}
+
+button,
+select,
+input {
+  padding: 1px 4px;
+
+  color: var(--text-color);
+  background-color: var(--body-color);
+  border: 2px solid var(--layout-color-secondary);
+  font-size: 15px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+button:hover,
+select:hover {
+  background-color: var(--layout-color-secondary);
 }
 </style>
